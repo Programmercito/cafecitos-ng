4a23ce0f17ce57fab170d92f37010d318e573948
programmercito <osboceo@live.com>
2025-09-05 17:32:53 -0400

    logos en svg

--- src/app/pages/auth/login.ts
+++ src/app/pages/auth/login.ts
@@ -7,19 +7,20 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { RippleModule } from 'primeng/ripple';
 import { AppFloatingConfigurator } from '../../layout/component/app.floatingconfigurator';
+import { Logo } from "@/shared/components/logo/logo";
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator],
+    imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator, Logo],
     template: `
         <app-floating-configurator />
         <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-screen overflow-hidden">
             <div class="flex flex-col items-center justify-center">
                 <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
                     <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
-                        <div class="text-center mb-8">
-                            <img src="images/logo.png" alt="Logo" width="90" height="90" class="mx-auto block"/>
+                        <div class="flex flex-col items-center text-center mb-8">
+                            <app-logo></app-logo>
                             <div class="text-surface-900 dark:text-surface-0 text-3xl font-medium mb-4">Sistema de Ordenes</div>
                             <span class="text-muted-color font-medium">Sign in to continue</span>
                         </div>
