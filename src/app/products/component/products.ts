f97983c69220f4a1a6787198158a052b337a42d4
programmercito <osboceo@live.com>
2025-09-09 00:53:48 -0400

    list products

--- src/app/products/component/products.ts
+++ src/app/products/component/products.ts
@@ -2,21 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { ToolbarModule } from "primeng/toolbar";
 import { ButtonModule } from "primeng/button";
 import { ProductsModel } from '@/libs/models/products-model';
+import { ProductsApiService } from '../services/products-api';
+import { TableModule } from "primeng/table";
+import { IconFieldModule } from "primeng/iconfield";
+import { InputIconModule } from "primeng/inputicon";
 
 @Component({
   selector: 'app-products',
-  imports: [ToolbarModule, ButtonModule],
+  imports: [ToolbarModule, ButtonModule, TableModule, IconFieldModule, InputIconModule],
   templateUrl: './products.html',
   styleUrl: './products.scss'
 })
 export class Products implements OnInit {
   current_product: ProductsModel = {} as ProductsModel;
   lista: ProductsModel[] = [];
-
-  constructor() {
-
+  active: boolean=true;
+  name: string = '';
+  page: number = 1;
+  perPage: number = 20;
+  constructor(private productService: ProductsApiService) {
   }
-  ngOnInit() {
+  ngOnInit(): void {
+      this.getProducts();
+  }
+  getProducts() {
+    this.productService.getProducts(this.active,this.name,this.page,this.perPage).subscribe({
+      next: (response) => {
+        console.log('Response:', response);
+        this.lista = response.data;
+        console.log('Lista:', this.lista);
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+      }
+    })
   }
 
   openNew() {
