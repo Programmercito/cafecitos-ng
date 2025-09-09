59b22721a0f3c1d11a646fa4c390c7b8f788c205
programmercito <osboceo@live.com>
2025-09-09 19:20:21 -0400

    fix pagination

--- src/app/libs/models/paginated-response.model.ts
+++ src/app/libs/models/paginated-response.model.ts
@@ -3,8 +3,7 @@ export interface Pagination {
     per_page: number;
     current_page: number;
     last_page: number;
-    from: number;
-    to: number;
+
 }
 
 export interface PaginatedResponse<T> {
--- src/app/products/component/products.ts
+++ src/app/products/component/products.ts
@@ -3,9 +3,10 @@ import { ToolbarModule } from "primeng/toolbar";
 import { ButtonModule } from "primeng/button";
 import { ProductsModel } from '@/libs/models/products-model';
 import { ProductsApiService } from '../services/products-api';
-import { TableModule } from "primeng/table";
+import { TableModule, TablePageEvent } from "primeng/table";
 import { IconFieldModule } from "primeng/iconfield";
 import { InputIconModule } from "primeng/inputicon";
+import { Pagination } from '@/libs/models/paginated-response.model';
 
 @Component({
   selector: 'app-products',
@@ -14,23 +15,43 @@ import { InputIconModule } from "primeng/inputicon";
   styleUrl: './products.scss'
 })
 export class Products implements OnInit {
+
   current_product: ProductsModel = {} as ProductsModel;
   lista: ProductsModel[] = [];
-  active: boolean=true;
+  active: boolean = true;
   name: string = '';
   page: number = 1;
-  perPage: number = 20;
+  perPage: number = 2;
+  pagination!: Pagination;
+  first: number = 0;
+  total!:number;
   constructor(private productService: ProductsApiService) {
   }
+
   ngOnInit(): void {
-      this.getProducts();
+    this.getProducts();
+  }
+  get currentPage(): number {
+    return Math.floor(this.first / this.perPage) + 1;
+  }
+  onPageChange($event: TablePageEvent) {
+    this.first = $event.first;
+    this.page = this.currentPage;
+    this.getProducts();
+
+
   }
   getProducts() {
-    this.productService.getProducts(this.active,this.name,this.page,this.perPage).subscribe({
+    this.productService.getProducts(this.active, this.name, this.page, this.perPage).subscribe({
       next: (response) => {
         console.log('Response:', response);
         this.lista = response.data;
+        this.pagination = response.pagination;
+        this.total = this.pagination.total;
+        //this.total=this.pagination.total;
         console.log('Lista:', this.lista);
+        console.log('Total:', this.total);
+        console.log('Pagination:', this.pagination);
       },
       error: (error) => {
         console.error('Error fetching products:', error);
