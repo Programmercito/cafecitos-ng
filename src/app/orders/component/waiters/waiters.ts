26c96efd8290ff30827e5278fecfbb3c68f5ebe5
programmercito <osboceo@live.com>
2025-09-12 17:04:45 -0400

    new pages

---
+++ src/app/orders/component/waiters/waiters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-waiters',
+  imports: [],
+  templateUrl: './waiters.html',
+  styleUrl: './waiters.scss'
+})
+export class Waiters {
+
+}
