02c2f3155ef0f5d86e788de966478c7770261fab
programmercito <osboceo@live.com>
2025-09-11 22:41:44 -0400

    new objects

--- src/app/libs/models/order.model.ts
+++ src/app/libs/models/order.model.ts
@@ -1,11 +1,34 @@
+import { ProductsModel } from "./products-model";
+import { UserModel } from "./user.model";
+
+export interface OrderWaiter {
+    id: number;
+    order_det_id: number;
+    waiter_id: number;
+    comision: number | null;
+    waiter: UserModel;
+}
+
+export interface OrderDetail {
+    id: number;
+    order_id: number;
+    product_id: number;
+    quantity: number;
+    price: string | null;
+    type: string;
+    observation: string;
+    product: ProductsModel;
+    order_waiters: OrderWaiter[];
+}
+
 export interface Order {
-  id: number;
-  status: string;
-  waiter?: {
-    username: string;
-  };
-  order_date: string; // ISO date string
-  price_final: number;
-  waiter_commission: number;
-  updated_at: string; // ISO date string
+    id: number;
+    order_date: string;
+    status: string;
+    waiter_id: number;
+    price_final: string;
+    waiter_comision: number | null;
+    updated_at: string | null;
+    details: OrderDetail[];
+    waiter: UserModel;
 }
