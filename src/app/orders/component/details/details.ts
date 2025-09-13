f9dd65815eafaebee8837a86db7706eb8f4e85af
programmercito <osboceo@live.com>
2025-09-13 01:48:13 -0400

    pantalla edicion de detalle

--- src/app/orders/component/details/details.ts
+++ src/app/orders/component/details/details.ts
@@ -7,16 +7,26 @@ import { FormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { TableModule } from "primeng/table";
 import { ButtonModule } from "primeng/button";
+import { DialogModule } from "primeng/dialog";
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-details',
-  imports: [TableModule, FormsModule, CurrencyPipe, ButtonModule],
+  imports: [TableModule, FormsModule, CurrencyPipe, ButtonModule, DialogModule, SelectModule],
   templateUrl: './details.html',
   styleUrl: './details.scss'
 })
 export class Details extends Common {
+  saveProduct() {
+  }
+  hideDialog() {
+  }
+  orderDetDialog: boolean = false;
+  currentDetail!: OrderDetail;
+  types: {name: string, value: string}[] = [];
   openNew() {
-    throw new Error('Method not implemented.');
+    this.currentDetail = {} as OrderDetail;
+    this.orderDetDialog = true;
   }
   details: OrderDetail[] = [];
   @Input() orderId!: number;
@@ -26,6 +36,10 @@ export class Details extends Common {
     super();
   }
   ngOnInit() {
+    this.getOrderDetails();
+    this.getDetailType();
+  }
+  getOrderDetails() {
     this.detailservice.getOrderDetails(this.orderId).subscribe({
       next: (response) => {
         this.details = response;
@@ -37,4 +51,22 @@ export class Details extends Common {
     });
   }
 
+  totalpay(): number {
+    let total = 0;
+    for (const detail of this.details) {
+      total += parseFloat(detail.price || '0');
+    }
+    return total;
+  }
+
+  getDetailType() {
+    this.detailservice.getProductTypes().subscribe({
+      next: (response) => {
+        this.types = response;
+      },
+      error: (error) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error fetching details types' });
+      }
+    });
+  }
 }
